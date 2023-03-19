b70d7898d06ee957d09be21b1cb60656eebfcf4a
Michael Plas <57052404+michaelplas@users.noreply.github.com>
2023-03-19 21:12:01 +0100

    test

--- main.js
+++ main.js
@@ -1,49 +1,56 @@
-const POSTS_FOLDER = 'posts/';
-const POSTS_EXTENSION = '.txt';
-const POSTS_LIMIT = 5;
-const NO_IMAGE_URL = 'https://picsum.photos/seed/no-image/300/200';
+const postSection = document.getElementById("post-section");
 
-// Get the recent posts container
-const recentPostsContainer = document.getElementById('recent-posts');
+// Function to fetch the recent posts from the posts.json file
+async function fetchPosts() {
+    try {
+        const response = await fetch("./posts.json");
+        const data = await response.json();
+        return data.posts;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-// Create a function to get the posts data from the server
-const getPostsData = async () => {
-    const response = await fetch(POSTS_FOLDER);
-    const text = await response.text();
-    const filenames = text.split('\n').filter(Boolean).filter((filename) => filename.endsWith(POSTS_EXTENSION));
-    const filesData = await Promise.all(filenames.slice(0, POSTS_LIMIT).map(async (filename) => {
-        const response = await fetch(`${POSTS_FOLDER}${filename}`);
-        const text = await response.text();
-        const [title, date, image, content] = text.split('\n');
-        return {
-            title,
-            date,
-            image,
-            content
-        };
-    }));
-    return filesData;
-};
+// Function to create a post card element
+function createPostCard(post) {
+    const postCard = document.createElement("div");
+    postCard.classList.add("card");
 
-// Create a function to generate the recent post cards
-const generateRecentPostCard = (postData) => {
-    const { title, image = NO_IMAGE_URL } = postData;
-    const card = document.createElement('div');
-    card.classList.add('card', 'mb-4', 'shadow-sm');
-    card.innerHTML = `
-    <img class="card-img-top" src="${image}" alt="${title}">
-    <div class="card-body">
-      <h5 class="card-title">${title}</h5>
-      <a href="${POSTS_FOLDER}${title.replace(/\s+/g, '-').toLowerCase()}${POSTS_EXTENSION}" class="btn btn-primary">Read more</a>
-    </div>
-  `;
-    return card;
-};
+    const postImage = document.createElement("img");
+    postImage.classList.add("card-img-top");
+    postImage.src = post.image ? post.image : "https://picsum.photos/500/300";
+    postImage.alt = post.title;
 
-// Populate the recent posts section with data from the server
-getPostsData().then((postsData) => {
-    postsData.forEach((postData) => {
-        const card = generateRecentPostCard(postData);
-        recentPostsContainer.appendChild(card);
+    const postCardBody = document.createElement("div");
+    postCardBody.classList.add("card-body");
+
+    const postTitle = document.createElement("h5");
+    postTitle.classList.add("card-title");
+    postTitle.textContent = post.title;
+
+    const postLink = document.createElement("a");
+    postLink.classList.add("btn", "btn-primary");
+    postLink.href = `./posts/${post.file}`;
+    postLink.textContent = "Read More";
+
+    postCardBody.appendChild(postTitle);
+    postCardBody.appendChild(postLink);
+
+    postCard.appendChild(postImage);
+    postCard.appendChild(postCardBody);
+
+    return postCard;
+}
+
+// Function to add the recent posts to the index.html page
+async function addRecentPosts() {
+    const posts = await fetchPosts();
+    const recentPosts = posts.slice(-3);
+
+    recentPosts.forEach((post) => {
+        const postCard = createPostCard(post);
+        postSection.appendChild(postCard);
     });
-});
+}
+
+addRecentPosts();
